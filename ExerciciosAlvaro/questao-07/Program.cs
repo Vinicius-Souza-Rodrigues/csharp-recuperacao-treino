f75a66b8e24d9df57385195d05911ef0fca3b9f5
Vinícius souza <vinicanal604@gmail.com>
2026-01-21 22:31:24 -0300

    feat/main: Adicionando questão 8 e atualizando questão 7

--- ExerciciosAlvaro/questao-07/Program.cs
+++ ExerciciosAlvaro/questao-07/Program.cs
@@ -3,15 +3,58 @@ decimal rendaMensal = 0m;
 int scoreCredito = 0;
 int scoreMinimo = 650;
 decimal limiteCredito = 0m;
-DateTime dataAtual = DateTime.Now;
+bool statusCliente = false;
+DateTime dataAtual = DateTime.Today;
 DateTime dataInicio;
-TimeSpan diferencaDataEmpregada = 0;
+int diferencaDataEmpregada;
 
-if (rendaMensal >= 4000 || (rendaMensal >= 2800 && diferencaDataEmpregada >= 6))
-    limiteCredito = 0.40;
 
-else if (rendaMensal >= 2000 && scoreCredito <= scoreMinimo && diferencaDataEmpregada >= 10)
-    limiteCredito = 0.25;
-    
+Console.WriteLine("Digite o nome do cliente: ");
+nomeCliente = Console.ReadLine();
+while (string.IsNullOrEmpty(nomeCliente))
+{
+    Console.WriteLine("Nome não pode ser vazio");
+    nomeCliente = Console.ReadLine();
+}
+
+Console.Clear();
+
+Console.WriteLine("Digite a renda mensal do cliente: ");
+while (!decimal.TryParse(Console.ReadLine(), out rendaMensal) || rendaMensal < 1)
+    Console.WriteLine("Renda mensal não pode ser convertida ou o valor esa incorreto");
+
+Console.Clear();
+
+Console.WriteLine("Digite o número de score que o cliente possui: ");
+while (!int.TryParse(Console.ReadLine(), out scoreCredito) || scoreCredito < 1)
+    Console.WriteLine("Renda mensal não pode ser convertida ou o valor esa incorreto");
+
+Console.Clear();
+
+Console.WriteLine("Digite a data em que o cliente começou: ");
+while (!DateTime.TryParse(Console.ReadLine(), out dataInicio) || dataInicio >= dataAtual)
+    Console.WriteLine("O valor digitado é invalido ou não pode ser convertido");
+
+Console.Clear();
+
+
+diferencaDataEmpregada = (dataAtual.Year - dataInicio.Year) * 12 + (dataAtual.Month - dataInicio.Month);
+
+if (rendaMensal >= 4000 || (rendaMensal >= 2800 && diferencaDataEmpregada >= 6)) {
+    limiteCredito = rendaMensal * 0.40m;
+    statusCliente = true;
+}
+else if (rendaMensal >= 2000 && scoreCredito <= scoreMinimo && diferencaDataEmpregada >= 10) {
+    limiteCredito = rendaMensal * 0.25m;
+    statusCliente = true;
+}
 else
-    limiteCredito = 0;
+    limiteCredito = 0m;
+
+Console.Write("=== ANÁLISE DE CRÉDITO === ");
+Console.Write($"Cliente: {nomeCliente} ");
+Console.Write($"Renda Mensal: {rendaMensal:C} ");
+Console.Write($"Score: {scoreCredito} ");
+Console.Write($"Tempo de Emprego: {diferencaDataEmpregada:d} meses ");
+Console.Write($"Status: {(statusCliente ? "Aprovado" : "Reprovado")} ");
+Console.Write($"Limite de Crédito: {(limiteCredito == 0 ? "Não possui limite de créditos" : limiteCredito):C} ");
---
+++ ExerciciosAlvaro/questao-08/Program.cs
@@ -0,0 +1,51 @@
+﻿List<decimal> listaValoresPedidos = [22.50m, 18.00m, 45.00m, 9.50m, 60.00m, 12.00m, 30.00m];
+
+decimal valorTotal = 0m;
+int quantidadProdutosPremium = 0;
+decimal vendaMaisCara = 0m;
+decimal vendaMaisBarata = 0m;
+decimal vendaMedia = 0m;
+int indice = 0;
+ClassificacaoDia classificacaoDia;
+
+listaValoresPedidos.ForEach(valor =>
+{
+    Console.WriteLine($"Venda {indice + 1}: {valor:C}\n");
+
+    if (valor > vendaMaisCara)
+        vendaMaisCara = valor;
+
+    if (valor < vendaMaisBarata || vendaMaisBarata == 0)
+        vendaMaisBarata = valor;
+
+    valorTotal += valor;
+
+    if (valor > 30)
+        quantidadProdutosPremium++;
+    indice++;
+});
+
+if (valorTotal >= 200)
+    classificacaoDia = ClassificacaoDia.Otimo;
+else if (valorTotal >= 120)
+    classificacaoDia = ClassificacaoDia.Bom;
+else if (valorTotal >= 60)
+    classificacaoDia = ClassificacaoDia.Regular;
+else
+    classificacaoDia = ClassificacaoDia.Fraco;
+
+Console.Write("=== RELATÓRIO DO DIA === ");
+Console.Write($"Faturamento Total: {valorTotal:C} ");
+if (quantidadProdutosPremium != 0)
+    Console.Write($"Pedidos premium: {quantidadProdutosPremium} ");
+Console.Write($"Maior Venda: {vendaMaisCara:C} ");
+Console.Write($"Menor Venda: {vendaMaisBarata:C} ");
+Console.Write($"Status do dia: {classificacaoDia} ");
+
+enum ClassificacaoDia
+{
+    Otimo = 1,
+    Bom = 2,
+    Regular = 3,
+    Fraco= 4,
+}
